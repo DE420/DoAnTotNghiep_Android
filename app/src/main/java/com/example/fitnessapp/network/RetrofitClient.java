b6ddba0bc105e5dd0f1a86c22556abc7a785a289
Vu Hong Tuyen <tuyenvu524@gmail.com>
2026-01-01 14:17:20 +0700

    commit before test community after translate to vietnamese

--- app/src/main/java/com/example/fitnessapp/network/RetrofitClient.java
+++ app/src/main/java/com/example/fitnessapp/network/RetrofitClient.java
@@ -16,7 +16,7 @@ public class RetrofitClient {
 //    private static final String BASE_URL = "http://10.0.2.2:8080/api/v1/";
 
     // ip address real device
-    private static final String BASE_URL = "http://192.168.1.168:8080/api/";
+    private static final String BASE_URL = "http://192.168.1.15:8080/api/";
 
 
     private static Retrofit retrofit = null;
